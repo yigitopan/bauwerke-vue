7815f2ed96f75301068fb8ccb962c3f8e671161d
yigitopan <yigitopan@hotmail.com>
2023-01-18 17:07:49 +0300

    vite config base changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/ngsi-start/',
+  base:'/bauwerke-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
